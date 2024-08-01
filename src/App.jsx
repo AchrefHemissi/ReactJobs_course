ae5ff95d6506250ebc8acc8429f1b7271adc3dc7
Mohammed Achref Hemissi <110178894+AchrefHemissi@users.noreply.github.com>
2024-08-01 02:55:45 +0100

    Deleting and updating a job feature

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage , {jobLoader} from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
-
+import EditJobPage from './pages/EditJobPage';
 
 
 
@@ -33,6 +33,18 @@ const App = () => {
         });
         return;
     };
+
+    const updateJob = async (job) => {
+        const res = await fetch(`/api/jobs/${job.id}`,{
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(job),
+        });
+        return;
+
+    };
     
     const router = createBrowserRouter(
         createRoutesFromElements(
@@ -41,6 +53,7 @@ const App = () => {
                 <Route path="/jobs" element={<JobsPage />} />
                 <Route path="/jobs/:id" element={<JobPage deleteJob={deleteJob} />} loader = {jobLoader}  />
                 <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
+                <Route path="/edit-job/:id" element={<EditJobPage updateJobSubmit={updateJob} />} loader = {jobLoader}  />
                 <Route path="*" element={<NotFoundPage />} />
             </Route>
         )
